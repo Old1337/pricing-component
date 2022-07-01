482415221e7aa1f87ef64e79327d2cbf857dcb3a
Arsenic-33 <omarahmed2441@gmail.com>
2022-07-01 02:28:38 +0200

    price component challenge

---
+++ Javascript/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const rangeBar = document.querySelector(".pricing-component_range-bar");
+const priceEl = document.querySelectorAll(".price");
+const durationEl = document.querySelectorAll(".duration");
+
+const toggleBtn = document.querySelector('input[type="checkbox"]');
+
+rangeBar.addEventListener("input", () => {
+  priceEl.forEach((el) => (el.textContent = `$${rangeBar.value}`));
+  if (rangeBar.dataset.yearly === "false") {
+    priceEl.forEach((el) => (el.textContent = `$${rangeBar.value * 2}`));
+  }
+  rangeBar.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ${
+    (rangeBar.value / 32) * 100
+  }%, hsl(224, 65%, 95%) ${
+    (rangeBar.value / 32) * 100
+  }%, hsl(224, 65%, 95%) 100%)`;
+});
+
+toggleBtn.addEventListener("click", () => {
+  if (rangeBar.dataset.yearly === "true") {
+    priceEl.forEach((el) => (el.textContent = `$${rangeBar.value * 2}`));
+    rangeBar.dataset.yearly = "false";
+  } else {
+    rangeBar.dataset.yearly = "true";
+
+    priceEl.forEach((el) => (el.textContent = `$${rangeBar.value}`));
+  }
+});
